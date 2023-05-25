2d1f443b0ee995facb2754d2007f5eb391d2465f
OmarKnows <omarknows98@gmail.com>
2023-05-25 12:04:16 +0200

    prepared components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './modules/login/login.component';
+import { LandingComponent } from './modules/landing/landing.component';
+import { PackagesComponent } from './modules/packages/packages.component';
+import { ProductsComponent } from './modules/products/products.component';
+import { StatisticsComponent } from './modules/statistics/statistics.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, LoginComponent, LandingComponent, PackagesComponent, ProductsComponent, StatisticsComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
