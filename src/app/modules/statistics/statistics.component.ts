f3ff8ee28a9015bbb650c294ca057acfd9be483b
OmarKnows <omarknows98@gmail.com>
2023-06-01 16:42:58 +0200

    adjusted graph

--- src/app/modules/statistics/statistics.component.ts
+++ src/app/modules/statistics/statistics.component.ts
@@ -1,7 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
 import { Chart, ChartConfiguration, ChartType, ChartOptions } from 'chart.js';
-import { BaseChartDirective } from 'ng2-charts';
-
 import { default as Annotation } from 'chartjs-plugin-annotation';
 
 @Component({
@@ -77,21 +75,33 @@ export class StatisticsComponent {
       line: {
         tension: 0.5,
       },
+      point: {
+        pointStyle: false,
+      },
     },
     scales: {
-      // We use this empty structure as a placeholder for dynamic theming.
       y: {
-        position: 'left',
-      },
-      y1: {
         position: 'right',
-        grid: {},
-        ticks: {},
+
+        grid: {
+          display: false,
+        },
+        ticks: {
+          callback: function (value, index, values) {
+            return 'ريال' + value;
+          },
+        },
+      },
+
+      x: {
+        grid: {
+          display: false,
+        },
       },
     },
 
     plugins: {
-      legend: { display: true },
+      legend: { display: false },
       annotation: {
         annotations: [
           {
@@ -115,44 +125,4 @@ export class StatisticsComponent {
   };
 
   public lineChartType: ChartType = 'line';
-
-  @ViewChild(BaseChartDirective) chart?: BaseChartDirective;
-
-  private static generateNumber(i: number): number {
-    return Math.floor(Math.random() * (i < 2 ? 100 : 1000) + 1);
-  }
-
-  public randomize(): void {
-    for (let i = 0; i < this.lineChartData.datasets.length; i++) {
-      for (let j = 0; j < this.lineChartData.datasets[i].data.length; j++) {
-        this.lineChartData.datasets[i].data[j] =
-          StatisticsComponent.generateNumber(i);
-      }
-    }
-    this.chart?.update();
-  }
-
-  public hideOne(): void {
-    const isHidden = this.chart?.isDatasetHidden(1);
-    this.chart?.hideDataset(1, !isHidden);
-  }
-
-  public pushOne(): void {
-    this.lineChartData.datasets.forEach((x, i) => {
-      const num = StatisticsComponent.generateNumber(i);
-      x.data.push(num);
-    });
-    this.lineChartData?.labels?.push(
-      `Label ${this.lineChartData.labels.length}`
-    );
-
-    this.chart?.update();
-  }
-
-  public changeColor(): void {
-    this.lineChartData.datasets[2].borderColor = 'green';
-    this.lineChartData.datasets[2].backgroundColor = `rgba(0, 255, 0, 0.3)`;
-
-    this.chart?.update();
-  }
 }
