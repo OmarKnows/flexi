4cf215752c20d14bbdf2fbf4ce49549d5380a311
OmarKnows <omarknows98@gmail.com>
2023-06-01 02:03:27 +0200

    fixed more bugs

--- src/app/modules/landing/landing.component.ts
+++ src/app/modules/landing/landing.component.ts
@@ -19,33 +19,10 @@ export class LandingComponent {
   public lineChartData: ChartConfiguration['data'] = {
     datasets: [
       {
-        data: [65, 59, 80, 81, 56, 55, 40],
-        label: 'Series A',
-        backgroundColor: 'rgba(148,159,177,0.2)',
-        borderColor: 'rgba(148,159,177,1)',
-        pointBackgroundColor: 'rgba(148,159,177,1)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgba(148,159,177,0.8)',
-        fill: 'origin',
-      },
-      {
-        data: [28, 48, 40, 19, 86, 27, 90],
-        label: 'Series B',
-        backgroundColor: 'rgba(77,83,96,0.2)',
-        borderColor: 'rgba(77,83,96,1)',
-        pointBackgroundColor: 'rgba(77,83,96,1)',
-        pointBorderColor: '#fff',
-        pointHoverBackgroundColor: '#fff',
-        pointHoverBorderColor: 'rgba(77,83,96,1)',
-        fill: 'origin',
-      },
-      {
-        data: [180, 480, 770, 90, 1000, 270, 400],
-        label: 'Series C',
-        yAxisID: 'y1',
-        backgroundColor: 'rgba(255,0,0,0.3)',
-        borderColor: 'red',
+        data: [31, 30, 80, 44, 56, 55, 80],
+        label: '',
+        backgroundColor: 'rgba(245, 255, 247, 1)',
+        borderColor: 'rgba(103, 179, 117, 1)',
         pointBackgroundColor: 'rgba(148,159,177,1)',
         pointBorderColor: '#fff',
         pointHoverBackgroundColor: '#fff',
@@ -53,7 +30,15 @@ export class LandingComponent {
         fill: 'origin',
       },
     ],
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    labels: [
+      '07 فبراير',
+      '06 فبراير',
+      '05 فبراير',
+      '04 فبراير',
+      '03 فبراير',
+      '02 فبراير',
+      '01 فبراير',
+    ],
   };
 
   public lineChartOptions: ChartConfiguration['options'] = {
@@ -69,12 +54,8 @@ export class LandingComponent {
       },
       y1: {
         position: 'right',
-        grid: {
-          color: 'rgba(255,0,0,0.3)',
-        },
-        ticks: {
-          color: 'red',
-        },
+        grid: {},
+        ticks: {},
       },
     },
 
@@ -85,8 +66,7 @@ export class LandingComponent {
           {
             type: 'line',
             scaleID: 'x',
-            value: 'March',
-            borderColor: 'orange',
+            borderColor: 'white',
             borderWidth: 2,
             label: {
               display: true,
